7655a0ca2e635cc6c78a887c6aac9198687710f5
mmm2404 <marina-marchishina@mail.ru>
2023-06-25 21:22:36 +0300

    some applications revoved

--- next.config.js
+++ next.config.js
@@ -2,13 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-  
         
-           
-            domains: ['books.google.com'],
-      
-        
-    
+       domains: ['books.google.com'],
+          
 },
   
 }
